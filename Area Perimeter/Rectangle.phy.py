2b1510ec113339c11d8732968d461dd2f627ab50
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-27 14:38:56 +0530

    Add files via upload

---
+++ Area Perimeter/Rectangle.phy.py
@@ -0,0 +1,6 @@
+l=int(input("Enter Length of Rectangle :: "))
+b=int(input("Enter Breadth of Rectangle :: "))
+area=l*b
+perimeter=2*(l+b)
+print("\nArea of Rectangle :: ",area)
+print("\nPerimeter of Rectangle :: ",perimeter)
