b3002da0eea21f0af89ea73f13319bd0b0d36b66
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-27 01:12:42 -0800

    Delete Arithmetic directory

--- Arithmetic/Div.phy.py
+++
@@ -1,4 +0,0 @@
-a=int(input("Enter First Number :: "))
-b=int(input("Enter Second Number :: "))
-ans=a/b
-print("Division Of ",a," & ",b,"is",ans)
--- Arithmetic/add.phy.py
+++
@@ -1,4 +0,0 @@
-a=float(input("Enter First Number :: "))
-b=float(input("Enter Second Number :: "))
-sum=a+b
-print("Addition Of ",a," & ",b,"is",sum)
