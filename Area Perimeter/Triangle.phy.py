ad047f5a5a6e5faee1b0bb6d114bc78b2e47e137
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-27 01:11:43 -0800

    Delete Area Perimeter/Triangle.phy.py

--- Area Perimeter/Triangle.phy.py
+++
@@ -1,8 +0,0 @@
-h=int(input("Enter height of Triangle :: "))
-a=int(input("Enter left side of Triangle :: "))
-b=int(input("Enter base of Triangle :: "))
-c=int(input("Enter Right side of Triangle :: "))
-area=0.5*h*b
-print("\nArea of Triangle is",area)
-perimeter=a+b+c
-print("\nPerimeter of Triangle is",perimeter)
