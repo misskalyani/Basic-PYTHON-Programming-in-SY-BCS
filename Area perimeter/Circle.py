0b54594a9be8d6e48a92fed0e0287ea4ec651a40
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-01-27 15:19:23 +0530

    Add files via upload

---
+++ Area perimeter/Circle.py
@@ -0,0 +1,6 @@
+from math import *
+r=float(input("Enter Radius of Circle :: "))
+area=pi*r*r
+circumference=2*pi*r
+print("\nArea of Circle is",area)
+print("\nCircumference of Circle is",circumference)
