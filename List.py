640f7d5f32aa593d200ea7593f1210811b157a4e
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-13 07:09:06 -0800

    Create List.py

---
+++ List.py
@@ -0,0 +1,68 @@
+list=[1,"sai",4567899865545,"om",8765432222]
+list1=[2,"sham",67898765678]
+
+print("list1",list1)
+print("list = ",list)
+
+print("list[1] = ",list[1])
+
+
+print("list[2:] = ",list[2:])
+
+print("list[:3] = ",list[:3])
+
+print("list[1:3] = ",list[1:3])
+
+print("list*2 = ",list*2)
+
+print("list+list1 = ",list+list1)
+
+print("Remove Om =")
+list.remove("om")
+print("list =",list)
+
+print("Append Kalyani=")
+list.append("kalyani")
+print("list =",list)
+
+print("Pop List =")
+list.pop()
+print("list =",list)
+
+print("Reverse List =")
+list.reverse()
+print("list =",list)
+
+print("Insert List =")
+list.insert(1,"om")
+print("list =",list)
+
+list3=["om","sai","ram","sham"]
+print("list3 =",list3)
+
+print(list3.(-3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
