49ffe58e06e0673236dcca0e4db1b6117fa47b4f
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-13 07:06:48 -0800

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+tuple=(1,"om","sai",786456567,787654456,)
+print("tuple = ",tuple)
+
+print("tuple[:2] =",tuple[:2])
+print("tuple[1:] =",tuple[1:])
+print("tuple[1] =",tuple[1])
+
+print("tuple[-1] =",tuple[-1])
+
+
