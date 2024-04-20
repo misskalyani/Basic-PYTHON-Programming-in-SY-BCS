93baee35460040053865468ee371ba1b787691dc
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-04-20 09:23:29 +0530

    Delete Maths_Python_Slips_Solution/Slip 1 directory

--- Maths_Python_Slips_Solution/Slip 1/Q3.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-items = ['clothing','food','rent','petrol','misc']
-exp = [600,4000,2000,1500,700]
-plt.bar(items,exp,width = 0.8,color = 'green')
-plt.xlabel('items')
-plt.ylabel('exp')
-plt.show()
--- Maths_Python_Slips_Solution/Slip 1/Q6.py
+++
@@ -1,8 +0,0 @@
-from sympy import *
-x,y = symbols('x y')
-a = Point(0,0)
-b = Point(5,0)
-c = Point(3,3)
-t = Triangle(a,b,c)
-print("Area : ",t.area)
-print("Perimeter :",t.perimeter)
